2a483a8e5bb1bddd830dffe18d7f0fa691c485f6
D4GNIR <jeremiecette@gmail.com>
2022-09-03 17:25:09 +0200

    modify error 404 page

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+export default function error404() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "5rem",
+          marginTop: "150px",
+          marginBottom: "10px",
+          textAlign: "center",
+          color: "#ee6c4d",
+        }}
+      >
+        404
+      </h1>
+      <p style={{ fontSize: "1.5rem" }}>Cette page n'existe pas</p>
+    </div>
+  );
+}
