96c9cb246feb38ed94686dc01147afafddaff3f2
D4GNIR <jeremiecette@gmail.com>
2022-09-07 10:02:44 +0200

    detecter la methode

--- pages/api/projet.js
+++ pages/api/projet.js
@@ -1,68 +1,73 @@
 import { connectToDatabase } from "../../helpers/mongodb";
 
 export default async function handler(req, res) {
-  const {
-    titre,
-    slug,
-    client,
-    annee,
-    description,
-    contenu,
-  } = req.body;
+  if (req.method == "POST") {
+    const {
+      titre,
+      slug,
+      client,
+      annee,
+      description,
+      contenu,
+    } = req.body;
 
-  //   Vérifier que tous les champs soit remplis
-  if (
-    !titre ||
-    !slug ||
-    !client ||
-    !annee ||
-    !description ||
-    !contenu
-  ) {
-    res.status(422).json({
-      message: "Champ du formulaire manquant.",
-    });
-    return;
-  }
-  //   Stocker le nouveau projet
-  const nouveauProjet = {
-    titre,
-    slug,
-    client,
-    annee,
-    description,
-    contenu,
-    dateDePublication: new Date(),
-  };
+    //   Vérifier que tous les champs soit remplis
+    if (
+      !titre ||
+      !slug ||
+      !client ||
+      !annee ||
+      !description ||
+      !contenu
+    ) {
+      res.status(422).json({
+        message: "Champ du formulaire manquant.",
+      });
+      return;
+    }
+    //   Stocker le nouveau projet
+    const nouveauProjet = {
+      titre,
+      slug,
+      client,
+      annee,
+      description,
+      contenu,
+      dateDePublication: new Date(),
+    };
 
-  //   Connection à MongoDB
-  let clientMongoDB;
+    //   Connection à MongoDB
+    let clientMongoDB;
 
-  try {
-    clientMongoDB = await connectToDatabase();
-  } catch (error) {
-    res.status(500).json({
-      message: "Impossible d'effectuer la requête.",
-    });
-    return;
-  }
+    try {
+      clientMongoDB = await connectToDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Impossible d'effectuer la requête.",
+      });
+      return;
+    }
 
-  const db = clientMongoDB.db();
+    const db = clientMongoDB.db();
 
-  //   Insérer un nouveau projet
-  try {
-    await db.collection("projets").insertOne(nouveauProjet);
-  } catch (error) {
+    //   Insérer un nouveau projet
+    try {
+      await db.collection("projets").insertOne(nouveauProjet);
+    } catch (error) {
+      clientMongoDB.close();
+      res.status(500).json({
+        message: "Un problème est survenu",
+      });
+    }
+
+    //   Succès
     clientMongoDB.close();
-    res.status(500).json({
-      message: "Un problème est survenu",
+    res.status(201).json({
+      message: "Projet ajouter avec succès",
+      projet: nouveauProjet,
     });
   }
-
-  //   Succès
-  clientMongoDB.close();
-  res.status(201).json({
-    message: "Projet ajouter avec succès",
-    projet: nouveauProjet,
+  res.status(405).json({
+    message: "Une erreur est survenue",
   });
 }
