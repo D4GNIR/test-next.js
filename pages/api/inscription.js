18822eb2542c5ac1ab5bd8e36fca918c1017ee6b
D4GNIR <jeremiecette@gmail.com>
2022-09-08 11:05:52 +0200

    api inscription ok

---
+++ pages/api/inscription.js
@@ -0,0 +1,98 @@
+import { connectToDatabase } from "../../helpers/mongodb";
+
+export default async function handler(req, res) {
+  // On vérifie qu'on recoit bien un post
+  if (req.method == "POST") {
+    // Toutes les infos qu'on recupere de l'api
+    const { pseudo, email, password } = req.body;
+    // On vérifie que tous les champs soit rempli
+    if (!pseudo || !email || !password) {
+      // Si il manque un élément
+      res.status(422).json({
+        message: "Champ du formulaire manquant.",
+      });
+      // On envoi return pour ne pas envoyer le reste du code
+      return;
+    }
+    //On Stocke le nouvel utilsateur
+    const nouvelUtilisateur = {
+      pseudo,
+      email,
+      password,
+    };
+
+    // Vérifier la syntaxe de l'email
+    const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!pattern.test(email)) {
+      res.status(406).json({
+        message: "Votre adresse email est invalide",
+      });
+      // On envoi return pour ne pas envoyer le reste du code
+      return;
+    }
+
+    //Connexion à MongoDB
+    let clientMongoDB;
+
+    try {
+      clientMongoDB = await connectToDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Impossible d'effectuer la requête.",
+      });
+      return;
+    }
+    // On recup la bdd
+    const db = clientMongoDB.db();
+    let emailDejaUtilise;
+
+    // Vérifier que l'adresse email n'est pas déjà utilisé
+    try {
+      emailDejaUtilise = await db
+        .collection("utilisateurs")
+        .findOne({ email: email });
+    } catch (error) {
+      // On ferme la bdd
+      clientMongoDB.close();
+      res.status(500).json({
+        message: "Un problème est survenu",
+      });
+      return;
+    }
+
+    if (emailDejaUtilise) {
+      clientMongoDB.close();
+      res.status(403).json({
+        message: "Cette adresse email est déjà utilisée",
+      });
+      return;
+    }
+
+    //Insérer un nouvel utilsateur
+    try {
+      // On injecte dans la table utilisateur le nouvel utilisateur
+      await db
+        .collection("utilisateurs")
+        .insertOne(nouvelUtilisateur);
+    } catch (error) {
+      // On ferme la bdd
+      clientMongoDB.close();
+      res.status(500).json({
+        message: "Un problème est survenu",
+      });
+      return;
+    }
+
+    //Succès
+    clientMongoDB.close();
+    res.status(201).json({
+      message: "Utilisateur ajouter avec succès",
+      utilisateur: nouvelUtilisateur,
+    });
+    // Si on ne recoit pas un post on envoi une erreur
+  } else {
+    res.status(405).json({
+      message: "Une erreur est survenue",
+    });
+  }
+}
--- pages/inscription/index.js
+++ pages/inscription/index.js
@@ -1,5 +1,7 @@
 import MyButton from "../../components/UI/Button/MyButton";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import Error from "../../components/UI/Error/Error";
 
 export default function Inscription() {
   const {
@@ -8,9 +10,31 @@ export default function Inscription() {
     formState: { errors },
   } = useForm();
 
+  // UseState
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [isRegister, setIsRegister] = useState(false);
+
   //   Methode
-  const onFormSubmit = (data) => {
-    console.log(data);
+  const onFormSubmit = async (data) => {
+    if (!isLoading) {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("/api/inscription", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const fetchedData = await response.json();
+
+      if (!response.ok) {
+        setIsLoading(false);
+        setError(fetchedData.message || "Une erreur est survenue");
+      } else {
+        setIsLoading(false);
+        setIsRegister(fetchedData.utilisateur);
+      }
+    }
   };
 
   return (
@@ -20,54 +44,64 @@ export default function Inscription() {
       </h1>
       <section style={{ display: "flex", justifyContent: "center" }}>
         <main style={{ backgroundColor: "#f3f3f3", padding: "30px" }}>
-          <form onSubmit={handleSubmit(onFormSubmit)}>
-            <p>
-              <label htmlFor='pseudo'>Pseudo</label>
-              <input
-                type='text'
-                placeholder='Pseudo'
-                {...register("pseudo", {
-                  required: true,
-                })}
-              />
-              {errors.pseudo && (
-                <small>Veuillez renseigner ce champ</small>
-              )}
-            </p>
-            <p>
-              <label htmlFor='email'>Addresse email</label>
-              <input
-                type='email'
-                placeholder='email'
-                {...register("email", {
-                  required: true,
-                  pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                })}
-              />
-              {errors.email && errors.email.type === "required" && (
-                <small>Veuillez renseigner ce champ</small>
-              )}
-              {errors.email && errors.email.type === "pattern" && (
-                <small>Votre adresse E-mail n'est pas correct</small>
-              )}
-            </p>
-            <p>
-              <label htmlFor='password'>Mot de passe</label>
-              <input
-                type='password'
-                placeholder='Mot de passe'
-                {...register("password", {
-                  required: true,
-                })}
-              />
-              {errors.password && (
-                <small>Veuillez renseigner ce champ</small>
-              )}
-            </p>
-            <div style={{ display: "flex", justifyContent: "end" }}>
-              <MyButton>Je m'inscris</MyButton>
+          {error && <Error>{error}</Error>}
+          {isRegister ? (
+            <div>
+              Félicitations ! Vous pouvez vous connecter
+              {isRegister.pseudo}.
             </div>
-          </form>
+          ) : (
+            <form onSubmit={handleSubmit(onFormSubmit)}>
+              <p>
+                <label htmlFor='pseudo'>Pseudo</label>
+                <input
+                  type='text'
+                  placeholder='Pseudo'
+                  {...register("pseudo", {
+                    required: true,
+                  })}
+                />
+                {errors.pseudo && (
+                  <small>Veuillez renseigner ce champ</small>
+                )}
+              </p>
+              <p>
+                <label htmlFor='email'>Addresse email</label>
+                <input
+                  type='email'
+                  placeholder='email'
+                  {...register("email", {
+                    required: true,
+                    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  })}
+                />
+                {errors.email && errors.email.type === "required" && (
+                  <small>Veuillez renseigner ce champ</small>
+                )}
+                {errors.email && errors.email.type === "pattern" && (
+                  <small>
+                    Votre adresse E-mail n'est pas correct
+                  </small>
+                )}
+              </p>
+              <p>
+                <label htmlFor='password'>Mot de passe</label>
+                <input
+                  type='password'
+                  placeholder='Mot de passe'
+                  {...register("password", {
+                    required: true,
+                  })}
+                />
+                {errors.password && (
+                  <small>Veuillez renseigner ce champ</small>
+                )}
+              </p>
+              <div style={{ display: "flex", justifyContent: "end" }}>
+                <MyButton>Je m'inscris</MyButton>
+              </div>
+            </form>
+          )}
         </main>
       </section>
     </>
