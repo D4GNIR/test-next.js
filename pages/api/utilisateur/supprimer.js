de8f373c7b7f80b2b5d9b9177bc2f242990f69c9
D4GNIR <jeremiecette@gmail.com>
2022-09-09 15:21:46 +0200

    delete work

--- pages/api/utilisateur/supprimer.js
+++ pages/api/utilisateur/supprimer.js
@@ -1,18 +1,53 @@
-import { connectToDatabase } from "../../helpers/mongodb";
+// Librairies
 import { getSession } from "next-auth/client";
+import { connectToDatabase } from "../../../helpers/mongodb";
 
-export async function handler(req, res) {
-  if (req.method == "DELETE") {
+export default async function handler(req, res) {
+  if (req.method === "DELETE") {
     const session = await getSession({ req: req });
 
     if (!session) {
-      res
-        .status(401)
-        .json({ message: "Impossible de vous authentifier." });
+      res.status(401).json({
+        message: "Impossible de vous authentifier",
+      });
       return;
     }
+
+    // Connexion à MongoDB
+    let clientMongoDB;
+    try {
+      clientMongoDB = await connectToDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Impossible de se connecter",
+      });
+      return;
+    }
+
+    const db = clientMongoDB.db();
+
+    // Supprimer l'utilisateur connecté
+    try {
+      await db.collection("utilisateurs").deleteOne({
+        email: session.user.email,
+      });
+    } catch (error) {
+      clientMongoDB.close();
+      res.status(501).json({
+        message: "Impossible de supprimer cet utilisateur",
+      });
+      return;
+    }
+
+    // Succès
+    res.status(200).json({
+      message: "Utilisateur supprimé avec succès",
+    });
+    return;
   } else {
-    res.status(403).json({ message: "Votre requête est invalidée" });
+    res.status(403).json({
+      message: "Votre requête est invalide",
+    });
     return;
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { getSession, signOut } from "next-auth/client";
 import { useState } from "react";
+import { SpinnerDotted } from "spinners-react";
 
 export default function Index(props) {
   const [isLoading, setIsLoading] = useState(false);
@@ -17,9 +18,7 @@ export default function Index(props) {
       //Envoyer la demande de suppression
       const response = await fetch("api/utilisateur/supprimer", {
         method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
       });
 
       const fetchedData = await response.json();
@@ -41,7 +40,7 @@ export default function Index(props) {
         Bienvenue
         {props.utilisateur
           ? `${props.utilisateur.name} sur mon portfolio`
-          : "sur mon portfolio"}
+          : " sur mon portfolio"}
       </h1>
       <div
         style={{
@@ -88,20 +87,29 @@ export default function Index(props) {
                 }}
                 onClick={onDeleteClick}
               >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  style={{ width: "15px", height: "15px" }}
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                {isLoading ? (
+                  <SpinnerDotted
+                    size={15}
+                    thickness={100}
+                    speed={100}
+                    color='#ffffff'
                   />
-                </svg>
+                ) : (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    style={{ width: "15px", height: "15px" }}
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                    />
+                  </svg>
+                )}
               </button>
             )}
           </p>
